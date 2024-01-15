23a4f5029151881633e44d75e967c1ae940c4316
Aji-09 <156678802+Aji-09@users.noreply.github.com>
2024-01-16 00:00:25 +0800

    Update header.js

--- common/header.js
+++ common/header.js
@@ -5,7 +5,7 @@ document.write(`
       <ul class="nav-links">
         <li><a href="./index.html">Home</a></li>
         <li><a href="//pages/about.html">About</a></li>
-        <li><a href="//pages/services.html">Services</a></li>
+        <li><a href="/../pages/services.html">Services</a></li>
         <li><a href="//pages/contact.html">Contact</a></li>
       </ul>
     </div>
