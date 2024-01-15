d9260603bc0bfd982e072df7a5ade9ab2722f87e
Aji-09 <156678802+Aji-09@users.noreply.github.com>
2024-01-15 22:45:24 +0800

    Add files via upload

---
+++ common/footer.js
@@ -0,0 +1,25 @@
+// footer.js
+document.write(`
+  <footer>
+    <div class="footer-links">
+      <a href="../index.html">Home</a>
+      <a href="../pages/about.html">About</a>
+      <a href="../pages/services.html">Services</a>
+      <a href="../pages/contact.html">Contact</a>
+    </div>
+
+    <div class="social-icons">
+      <img src="../assets/image/facebook.png" alt="Facebook" />
+      <img src="../assets/image/twitter.png" alt="Twitter" />
+      <img src="../assets/image/instagram.png" alt="Instagram" />
+    </div>
+
+    <div class="additional-info">
+      <p>Email: cjeyvillanueva@gmail.com</p>
+      <p>Phone: +639978692760</p>
+      <p>Carissa, Bagtas, Tanza, Cavite</p>
+    </div>
+
+    <p class="cpryt">&copy; 2024 CJEY. All rights reserved.</p>
+  </footer>
+`);
---
+++ common/header.js
@@ -0,0 +1,17 @@
+document.write(`
+  <header>
+    <a class="logo" href="index.html"><img src="../assets/image/logo.png" width="100"/></a>
+    <div>
+      <ul class="nav-links">
+        <li><a href="../index.html">Home</a></li>
+        <li><a href="../pages/about.html">About</a></li>
+        <li><a href="../pages/services.html">Services</a></li>
+        <li><a href="../pages/contact.html">Contact</a></li>
+      </ul>
+    </div>
+    <div class="sign">
+      <button><a href="../pages/login.html">Sign in</a></button>
+      <button><a href="../pages/login.html">Sign up</a></button>
+    </div>
+  </header>
+`);
