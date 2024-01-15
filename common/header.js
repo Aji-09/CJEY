8e9ac0143dc8f081e72da2e23017ae8b7649a3a2
Aji-09 <156678802+Aji-09@users.noreply.github.com>
2024-01-15 23:56:24 +0800

    Update header.js

--- common/header.js
+++ common/header.js
@@ -1,11 +1,11 @@
 document.write(`
   <header>
-    <a class="logo" href="index.html"><img src="/assets/image/logo.png" width="100"/></a>
+    <a class="logo" href="index.html"><img src="//assets/image/logo.png" width="100"/></a>
     <div>
       <ul class="nav-links">
         <li><a href="./index.html">Home</a></li>
-        <li><a href="/pages/about.html">About</a></li>
-        <li><a href="/pages/services.html">Services</a></li>
+        <li><a href="//pages/about.html">About</a></li>
+        <li><a href="//pages/services.html">Services</a></li>
         <li><a href="//pages/contact.html">Contact</a></li>
       </ul>
     </div>
